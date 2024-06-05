baa35ab2f7edf309bcdeb41e075702e2289603a7
Samuel Legrain <samuel.legrain@bstorm.be>
2024-06-05 16:21:51 +0200

    Ajout démonstration 04 : L'encapsulation

---
+++ Demo04-Encapsulation/Login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo04_Encapsulation
+{
+    internal class Login
+    {
+        private string _email;
+        private string _password;
+
+        public void SetEmailAndPassword(string email, string password)
+        {
+            _email = email;
+            _password = password;
+        }
+
+        public bool CheckLogin(string email, string password)
+        {
+            return _password == password && _email == email;
+        }
+    }
+}
---
+++ Demo04-Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Demo04_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login compteBstorm = new Login();
+            compteBstorm.SetEmailAndPassword("sam@fait.rire", "Test1234=");
+            if(compteBstorm.CheckLogin("a@a","toto"))
+            {
+                Console.WriteLine("Bienvenue!");
+            }
+            else
+            {
+                Console.WriteLine("Mauvais identifiant...");
+            }
+        }
+    }
+}
