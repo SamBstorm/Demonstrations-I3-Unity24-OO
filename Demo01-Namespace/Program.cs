4ea6df26fdec48f404f3066e1fef174d35237bea
Samuel Legrain <samuel.legrain@bstorm.be>
2024-06-05 09:27:13 +0200

    Ajoutez des fichiers projet.

---
+++ Demo01-Namespace/Program.cs
@@ -0,0 +1,19 @@
+﻿using Demo01_Namespace.Enums;
+
+namespace Demo01_Namespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez piocher une carte :");
+            Console.WriteLine("Appuyez sur Enter pour piocher...");
+            Console.ReadLine();
+            Random RNG = new Random();
+            Carte maCarte;
+            maCarte.valeur = Enum.GetValues<Valeurs>()[RNG.Next(0,14)];
+            maCarte.couleur = Enum.GetValues<Couleurs>()[RNG.Next(0,4)];
+            Console.WriteLine($"Votre carte était : Le {maCarte.valeur} de {maCarte.couleur}");
+        }
+    }
+}
---
+++ Demo01-Namespace/Structs.cs
@@ -0,0 +1,15 @@
+﻿using Demo01_Namespace.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01_Namespace
+{
+    public struct Carte
+    {
+        public Couleurs couleur;
+        public Valeurs valeur;
+    }
+}
