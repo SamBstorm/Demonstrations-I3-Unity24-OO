b13d168e54b5d5041b37db8e2cda28f0ac060bb1
Samuel Legrain <samuel.legrain@bstorm.be>
2024-06-06 12:15:41 +0200

    Ajout Démonstrations 05 : les Propriétés

---
+++ Demo05-Proprietes/Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Demo05_Proprietes
+{
+    public class Login
+    {
+        #region champs
+        private string _email;
+        private string _password;
+        #endregion
+        #region propriétés
+        public string Email
+        {
+            private get { return _email; }
+            set
+            {
+                value = value.Trim().ToLower();
+                if (value.Contains('@') && value.Length <= 320 && value.Length >= 3)
+                {
+                    _email = value;
+                }
+                //SI on entre PAS dans la condition précédente, il serait plus judicieux de "Lancer une Exception" (Gestion des erreurs C#)
+            }
+        }
+        public string Password
+        {
+            private get { return _password; }
+            set
+            {
+                if (value.Length < 8) return;
+                if (value.Length > 32) return;
+                if (!Regex.IsMatch(value, "[a-z]+")) return;  //Vérifie si il y a une minuscule 
+                if (!Regex.IsMatch(value, "[A-Z]+")) return;  //Vérifie si il y a une majuscule 
+                if (!Regex.IsMatch(value, "[0-9]+")) return;  //Vérifie si il y a un chiffre
+                if (!Regex.IsMatch(value, @"[\-\.@#=+/\\]+")) return;  //Vérifie si il y a un symbole
+                _password = value;
+            }
+        } 
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int YearsOld {
+            get
+            {
+                DateTime aujourdhui = DateTime.Today;
+                int age = aujourdhui.Year - BirthDate.Year;
+                if (BirthDate.Date > aujourdhui.AddYears(-age)) age--;
+                return age;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Demo05-Proprietes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo05_Proprietes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login user1 = new Login();
+            Console.WriteLine("INSCRIPTION :");
+            Console.WriteLine("E-Mail:");
+            user1.Email = Console.ReadLine();
+            Console.WriteLine("Mot de passe :");
+            user1.Password = Console.ReadLine();
+            Console.WriteLine($"Utilisateur 1 : Votre email et mot de passe sont maintenant sauvegardé!");
+        }
+    }
+}
