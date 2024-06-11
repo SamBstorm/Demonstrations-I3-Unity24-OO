49686e4e9df3d2ccadc038c7193551cd7ee3f48c
Samuel Legrain <samuel.legrain@bstorm.be>
2024-06-11 10:14:53 +0200

    Ajout Démonstration 07 : Les opérateurs

---
+++ Demo07-Operateurs/Personne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo07_Operateurs
+{
+    public class Personne
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Personne Father { get; private set; }
+        public Personne Mother { get; private set; }
+
+        public Personne(string firstName, string lastName) {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public static Personne operator +(Personne mother, Personne father) { 
+            Personne enfant = new Personne(
+                $"Enfant de la famille {mother.LastName}-{father.LastName}",
+                father.LastName);
+            enfant.Mother = mother;
+            enfant.Father = father;
+            return enfant;
+        }
+
+        public static bool operator ==(Personne left, Personne right)
+        {
+            return left.FirstName == right.FirstName && left.LastName == right.LastName;
+        }
+
+        public static bool operator !=(Personne left, Personne right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ Demo07-Operateurs/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Demo07_Operateurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne("Alfred", "Daudet");
+            Personne p2 = new Personne("Mary", "Poppins");
+
+            Console.WriteLine($"Voici une première personne : {p1.FirstName} {p1.LastName}");
+            Console.WriteLine($"Voici une seconde personne : {p2.FirstName} {p2.LastName}");
+
+            Console.WriteLine("Leur union permet d'obtenir une troisième personne :");
+            Personne p3 = p2 + p1;
+
+            Console.WriteLine($"{p3.FirstName} {p3.LastName}");
+            Console.WriteLine($"Il sait que sa mère est = {p3.Mother.FirstName} {p3.Mother.LastName}");
+            Console.WriteLine($"Il sait que son père est = {p3.Father.FirstName} {p3.Father.LastName}");
+
+            //p3 correspond à l'enfant de p1 et p2
+
+            Console.WriteLine("Veuillez donner le prénom d'une personne recherchée :");
+            string prenom = Console.ReadLine();
+            Console.WriteLine("Et son nom de famille :");
+            string nom = Console.ReadLine();
+            Personne personneRecherchee = new Personne(prenom, nom);
+
+            if(p1 == personneRecherchee) { 
+                Console.WriteLine($"Nous l'avons trouvé, c'est p1!"); 
+            }
+            else if(p2 == personneRecherchee) { 
+                Console.WriteLine($"Nous l'avons trouvé, c'est p2!"); 
+            }
+            else if(p3 == personneRecherchee) { 
+                Console.WriteLine($"Nous l'avons trouvé, c'est p3!"); 
+            }
+            else
+            {
+                Console.WriteLine("Personne ne correspond...");
+            }
+        }
+    }
+}
