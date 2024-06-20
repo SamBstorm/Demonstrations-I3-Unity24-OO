0d9267d51a58b2fb3ea71f1fa14534cc11f58923
Samuel Legrain <samuel.legrain@bstorm.be>
2024-06-20 08:38:58 +0200

    Ajout Démonstration 12 : Les délégués

---
+++ Demo12-Deleguate01/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Demo12_Deleguate01
+{
+    public delegate void ValeurAleatoireDelegate(int val1, int val2);
+    internal class Program
+    {
+        static Random RNG = new Random();
+        static void Main(string[] args)
+        {
+            int nbLances;
+            do
+            {
+                Console.WriteLine("Combien de lancé de dé?");
+            } while (!int.TryParse(Console.ReadLine(), out nbLances));
+
+            ValeurAleatoireDelegate lances = null;
+            for (int i = 0; i < nbLances; i++)
+            {
+                if (lances is null) lances = LancerDe;
+                else lances += LancerDe;
+            }
+
+            //if(!(lances is null)) {
+            //    lances(1,6);
+            //}
+
+            lances?.Invoke(1,6);
+        }
+
+        static void LancerDe(int min, int max)
+        {
+            Console.WriteLine($"Le lancé vaut : {RNG.Next(min, max + 1)}");
+        }
+    }
+}
